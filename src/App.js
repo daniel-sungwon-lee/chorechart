9786c019fdab82a03a4ec0ddad13d07dc504e58b
Daniel Sungwon Lee <danlee333@gmail.com>
2021-07-12 21:17:33 -0700

    routed home

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './home';
 
 function App() {
   return (
     <div className="App">
+
+      <Router>
+
+        <Switch>
+
+          <Route exact path="/">
+            <Home/>
+          </Route>
+
+        </Switch>
+
+      </Router>
 
     </div>
   );
