a513a45b335ffa225e3c3248c8186196cf3632a8
Daniel Sungwon Lee <danlee333@gmail.com>
2021-07-27 12:53:15 -0700

    added header transition

--- src/home.jsx
+++ src/home.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { gsap } from 'gsap';
 
 export default function Home (props) {
+    const header = React.createRef();
+    
+    useEffect(() => {
+        gsap.to(header.current, {color: "#7fabfc", duration: 1.5, scale: 1.1, y: "2rem"})
+    }, [header])
+
     return (
         <div className="container" style={{paddingBottom: "6rem"}}>
-            <h1>Chore Chart</h1>
+            <h1 ref={header}>Chore Chart</h1>
             
         </div>
     )
