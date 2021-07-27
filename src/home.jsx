451e369d04c5870dd7e77d8470a5f80b561c0602
Daniel Sungwon Lee <danlee333@gmail.com>
2021-07-27 11:58:48 -0700

    added gsap

--- src/home.jsx
+++ src/home.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 
 export default function Home (props) {
     return (
-        <div>
+        <div className="container" style={{paddingBottom: "6rem"}}>
+            <h1>Chore Chart</h1>
             
         </div>
     )
